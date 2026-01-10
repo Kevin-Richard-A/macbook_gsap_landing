6d271911a547fb7d08a9a5ca02990460d043a444
Kevin-Richard-A <richard01kevin@gmail.com>
2026-01-10 20:15:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
+import ProductViewer from "./components/ProductViewer.jsx";
+import gsap from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const App = () => {
+    return (
+        <main>
+            <NavBar/>
+            <Hero />
+            <ProductViewer />
+        </main>
+    )
+}
+export default App
